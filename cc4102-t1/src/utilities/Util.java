bef7a6f7ec0c124da7822c67919c424f48aa290a
Diego Salazar L <dsalazarl@gmail.com>
2018-10-26 18:51:58 -0300

    Se agrega en utilities un método para imprimir en consola un arreglo de nodos, con fines de testing.

--- cc4102-t1/src/utilities/Util.java
+++ cc4102-t1/src/utilities/Util.java
@@ -1,8 +1,18 @@
 package utilities;
 
+import misql.Node;
+
+import java.util.ArrayList;
+
 public class Util {
+
     public static void HelloWorld() {
         System.out.print("Güena!" + '\n');
+    }
+
+    public static void printArrayList(ArrayList<Node> nodeList) {
+        for (Node node : nodeList)
+            System.out.println(node.oneLine());
     }
 
 }
