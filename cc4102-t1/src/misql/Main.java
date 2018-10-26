aa877a9cf696991adf64e906a159649bd12ceafc
Diego Salazar L <dsalazarl@gmail.com>
2018-10-26 19:39:16 -0300

    Se crea un nuevo generador aleatorio de nodos. Se crea el método para generar nodos de tipo Producto. Se ajusta el Main para probarlo.

--- cc4102-t1/src/misql/Main.java
+++ cc4102-t1/src/misql/Main.java
@@ -1,11 +1,18 @@
 package misql;
 
+import utilities.NodeGenerator;
+import utilities.Util;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Node myNode = new Node();
-        myNode.insert("id", "1");
-        myNode.insert("puntos", "34553");
-        System.out.println(myNode.oneLine());
+        /* Testing of random generation of nodes of type Product */
+        NodeGenerator ng = new NodeGenerator();
+        int numberOfNodes = (int) Math.pow(10,2);
+        System.out.println("- Generating " + Integer.toString(numberOfNodes) + " nodes -");
+        ArrayList<Node> randomProducts = ng.generateProductNodes(numberOfNodes);
+        Util.printArrayList(randomProducts);
     }
 }
