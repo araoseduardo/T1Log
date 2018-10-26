68fd042d62b3087a669926e8e5e600be108eb942
Diego Salazar L <dsalazarl@gmail.com>
2018-10-26 00:35:00 -0300

    Se agrega el proyecto java, básico con HelloWorld. Se mueve el archivo gitignore al interior de la carpeta del proyecto java.

---
+++ cc4102-t1/src/misql/Main.java
@@ -0,0 +1,9 @@
+package misql;
+
+import utilities.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        Util.HelloWorld();
+    }
+}
---
+++ cc4102-t1/src/utilities/Util.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Util {
+    public static void HelloWorld() {
+        System.out.println("Wena!");
+    }
+
+}
