1a2e4717e6187c4e19063ef1df2abc1f8b70b1da
Diego Salazar L <dsalazarl@gmail.com>
2018-10-28 19:01:33 -0300

    Se crea nueva clase de Nodos de tipo Producto. Se crea un nuevo método para generar nodos aleatorios y guardarlos directamente en archivos. Se actualiza el Main haciéndolo más interactivo y con medición de tiempo de ejecución.

--- cc4102-t1/src/misql/Main.java
+++ cc4102-t1/src/misql/Main.java
@@ -5,22 +5,67 @@ import utilities.Util;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    private static void productNodes() throws IOException {
-        String fileName = "data/input/random_product_nodes.txt";
-        NodeGenerator ng = new NodeGenerator();
-        int numberOfNodes = (int) Math.pow(10,3);
-        System.out.println("- Generating " + Integer.toString(numberOfNodes) + " nodes -");
-        ArrayList<Node> randomProducts = ng.generateProductNodes(numberOfNodes);
+    /**
+     * Generates random nodes of type Product by using an ArrayList and then saving the array's content into a File.
+     * @throws IOException
+     */
+    private static void productNodesArrayToFile(String fileName, NodeGenerator ng, int numberOfNodes) throws IOException {
+        System.out.println("\n* Generating Array of " + Integer.toString(numberOfNodes) + " nodes *");
+        ArrayList<Node> randomProducts = ng.generateProductNodesArray(numberOfNodes);
         Util.saveNodesToFile(fileName, randomProducts);
-        System.out.println("\nThe nodes were saved to file '" + fileName + "' .\n");
+        System.out.println("The nodes were saved to file '" + fileName + "' .");
         //Util.printArrayList(randomProducts);
     }
 
+
+    /**
+     * Generates random nodes of type Product saving them directly into a File.
+     * @throws IOException
+     */
+    private static void productNodesFile(String fileName, NodeGenerator ng, int numberOfNodes) throws IOException {
+        System.out.println("\n* Generating " + Integer.toString(numberOfNodes) + " nodes *");
+        ng.generateProductNodesToFile(numberOfNodes, fileName);
+        System.out.println("The nodes were saved to file '" + fileName + "' .");
+    }
+
     public static void main(String[] args) throws IOException {
-        /* Testing of random generation of nodes of type Product */
-        productNodes();
+        /* Setting initial data */
+        String fileName = "data/input/random_product_nodes.txt";
+        long startTime, stopTime, firstElapsedTime, secondElapsedTime;
+        NodeGenerator ng = new NodeGenerator();
+        int numberOfNodes;
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        while (true) {
+            System.out.println("Enter the number of the power of 10 to generate nodes: ");
+            int n = reader.nextInt();
+            if (n < 0 || n > 7)
+                System.out.println("\nPlease consider only numbers from 0 to 7\n");
+
+            numberOfNodes = (int) Math.pow(10, n);
+            break;
+        }
+        reader.close();
+
+        /*
+        // Testing of random generation of nodes of type Product <ArrayList approach>
+        startTime = System.currentTimeMillis();
+        productNodesArrayToFile(fileName, ng, numberOfNodes);
+        stopTime = System.currentTimeMillis();
+        firstElapsedTime = stopTime - startTime;
+        System.out.println("--Elapsed time: " + firstElapsedTime + "\n");
+        */
+
+        /* Testing of random generation of nodes of type Product <DirectToFile approach> */
+        startTime = System.currentTimeMillis();
+        productNodesFile(fileName, ng, numberOfNodes);
+        stopTime = System.currentTimeMillis();
+        secondElapsedTime = stopTime - startTime;
+        System.out.println("--Elapsed time: " + secondElapsedTime + "\n");
+
     }
 }
--- cc4102-t1/src/utilities/NodeGenerator.java
+++ cc4102-t1/src/utilities/NodeGenerator.java
@@ -1,47 +1,42 @@
 package utilities;
 
 import misql.Node;
+import misql.ProductNode;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 
 /**
  * This class will be used for experimental purposes, to generate random input data.
  */
 public class NodeGenerator {
-    private Random random;
-
-    public NodeGenerator() {
-        random = new Random();
-    }
-
-    private int randomIntBetween(int min, int max) {
-        return random.nextInt((max - min) + 1) + min;
-    }
 
     /**
      * This method will randomly generate a set of Node of type Product.
      * @param size is the amount of generated nodes desired.
      * @return an ArrayList<Node> with the generated product-nodes.
      */
-    public ArrayList<Node> generateProductNodes(int size) {
+    public ArrayList<Node> generateProductNodesArray(int size) {
         // Note: the id will be generated with incremental numbers so it does not contain duplicated
         // ids, but the list will be returned shuffled.
 
         ArrayList<Node> nodes = new ArrayList<>();
-        for (int i=1; i <= size; i++) {
-            Node node = new Node();
-            node.insert("id", Integer.toString(i));
-            int precio = randomIntBetween(10000, 10000000);
-            node.insert("precio", Integer.toString(precio));
-            node.insert("puntosNec", Long.toString(Math.round(precio*0.0005)));
-            node.insert("puntosRec", Long.toString(Math.round(precio*0.000025))); // Each 20 purchased, exchange one free.
-
-            nodes.add(node);
-        }
+        for (int i=1; i <= size; i++)
+            nodes.add(ProductNode.randomProductNode(i));
 
         Collections.shuffle(nodes);
         return nodes;
+    }
+
+    public void generateProductNodesToFile(int size, String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, false));
+
+        for (int i=1; i <= size; i++)
+            writer.append(ProductNode.randomProductNode(i).oneLine()+"\n");
+
+        writer.close();
     }
 }
